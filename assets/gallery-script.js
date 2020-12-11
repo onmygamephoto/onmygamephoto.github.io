0a47eec8b48be709cd2f962018e6888b454dc8ba
Dana <dlol@stanford.edu>
2020-12-11 14:49:13 -0800

    fix csv path

--- assets/gallery-script.js
+++ assets/gallery-script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 $.ajax({
   type: "GET",
-  url: "gallery-info.csv",
+  url: "assets/gallery-info.csv",
   // url: "https://raw.githubusercontent.com/dlansigan/test-csv/main/gallery-info.csv?token=AD37W4Y7LJLDJBIWXLJUO4S73ORUU",
   async: false,
   dataType: "text",
