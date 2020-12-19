285053aad4ad78f7c9ef8efea46adfd5b8e759f9
Dana <dlol@stanford.edu>
2020-12-19 13:52:43 -0800

    add carousel on splash

--- assets/splash-script.js
+++ assets/splash-script.js
@@ -1,4 +1,41 @@
+var interval1;
+var timer1;
+var interval2;
+var timer2;
+var interval3;
+var timer3;
+
  $(document).ready(function () {
    var overlay = document.getElementById("overlay");
    $(overlay).delay(800).fadeOut(1500);
+
+
+   $('#box-1 .carousel-img:gt(0)').hide(); // to hide all but the first image when page loads
+   $('#box-2 .carousel-img:gt(0)').hide(); // to hide all but the first image when page loads
+   $('#box-3 .carousel-img:gt(0)').hide(); // to hide all but the first image when page loads
+
+   timer1 = function(){
+   interval1=setInterval(function () {
+       $('#box-1 .carousel-img:first-child').fadeOut(1000)
+           .next().fadeIn(1000).end().appendTo('#box-1');
+   }, 5000);
+   };
+
+   timer2 = function(){
+   interval2=setInterval(function () {
+       $('#box-2 :first-child').fadeOut(1000)
+           .next().fadeIn(1000).end().appendTo('#box-2');
+   }, 5000);
+   };
+
+   timer3 = function(){
+   interval3=setInterval(function () {
+       $('#box-3 :first-child').fadeOut(1000)
+           .next().fadeIn(1000).end().appendTo('#box-3');
+   }, 5000);
+   };
+
+   timer1();
+   setTimeout(function(){timer2()},1000);
+   setTimeout(function(){timer3()},2000);
  });
